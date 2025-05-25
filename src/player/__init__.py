063f83f299fb593fc5fd154a887aba84cfda78bb
Cem Yildiz <cem.yildiz@rightnow.de>
2025-05-25 10:18:02 +0200

    play/pause button icon library

--- src/player/__init__.py
+++ src/player/__init__.py
@@ -1,5 +1,8 @@
 import vlc
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QFrame
+import qtawesome as qta
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QFrame, QHBoxLayout, QToolBar
+
+from .play_pause_button import PlayPauseButton
 
 class Player(QWidget):
     def __init__(self, parent=None):
@@ -13,8 +16,25 @@ class Player(QWidget):
         self.videoFrame = QFrame()
         self.playerLayout.addWidget(self.videoFrame)
 
-        self.mediaPlayer = self.instance.media_player_new()
-        self.mediaPlayer.set_nsobject(self.videoFrame.winId())
+        self.mediaPlayer = self.createMediaPlayer()
+        self.playerControls = self.createPlayerControls()
+        self.playerLayout.addWidget(self.playerControls)
+
+
+
+    def createMediaPlayer(self):
+        mediaPlayer = self.instance.media_player_new()
+        mediaPlayer.set_nsobject(self.videoFrame.winId())
+        return mediaPlayer
+
+    def createPlayerControls(self):
+
+        toolBar = QToolBar()
+
+        self.playPauseButton = PlayPauseButton(toolBar, self.mediaPlayer.play, self.mediaPlayer.pause)
+
+        return toolBar
+
 
     def play(self, url:str):
         self.media = self.instance.media_new(url)
@@ -23,3 +43,4 @@ class Player(QWidget):
         print('get_meta',self.media.get_meta(0))
         self.mediaPlayer.set_media(self.media)
         self.mediaPlayer.play()
+        self.playPauseButton.setStatus("playing")
---
+++ src/player/play_pause_button.py
@@ -0,0 +1,59 @@
+import types
+
+import qtawesome as qta
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QToolBar
+
+class PlayPauseButton:
+
+    def __init__(self, toolBar: QToolBar = None, playFunction:types.FunctionType=None, pauseFunction:types.FunctionType=None):
+        self.__init__(toolBar, playFunction, pauseFunction, "paused", "enabled")
+
+    def __init__(self, toolBar: QToolBar = None, playFunction:types.FunctionType=None, pauseFunction:types.FunctionType=None, status="paused", state="disabled"):
+        self.playIcon = qta.icon("fa6s.play")
+        self.pauseIcon = qta.icon("fa6s.pause")
+
+        self.toolBar = toolBar
+        self.action = self.toolBar.addAction(self.playIcon, "Play")
+        self.action.triggered.connect(self.call)
+
+        if toolBar is None:
+            raise Exception("plesase set toolBar")
+        if playFunction is None:
+            raise Exception("plesase set playFunction")
+        if pauseFunction is None:
+            raise Exception("plesase set pauseFunction")
+
+        self.status = status
+        self.state = state
+        self.playFunction = playFunction
+        self.pauseFunction = pauseFunction
+
+
+    def getAction(self):
+        return self.action
+
+    def setStatus(self, status: str):
+        self.status = status
+        if status == "playing":
+            self.action.setIcon(self.playIcon)
+        elif status == "paused":
+            self.action.setIcon(self.pauseIcon)
+
+    def setState(self, state: str):
+        self.state = state
+        if state == "enabled":
+            self.setEnabled()
+        else:
+            self.setDisabled()
+
+    def call(self):
+        if self.state == "enabled":
+            if self.status == "playing" and self.playFunction is not None:
+                self.pauseFunction()
+                self.setStatus("paused")
+            else:
+                if self.playFunction is not None:
+                    self.playFunction()
+                    self.setStatus("playing")
+
