eff20a4cff17dda076b7740b866f31cefb57c80f
Cem Yildiz <cem.yildiz@rightnow.de>
2025-05-25 08:46:41 +0200

    init project

---
+++ src/player/__init__.py
@@ -0,0 +1,25 @@
+import vlc
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QFrame
+
+class Player(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setObjectName("Player")
+
+        self.instance = vlc.Instance()
+
+
+        self.playerLayout = QVBoxLayout(self)
+        self.videoFrame = QFrame()
+        self.playerLayout.addWidget(self.videoFrame)
+
+        self.mediaPlayer = self.instance.media_player_new()
+        self.mediaPlayer.set_nsobject(self.videoFrame.winId())
+
+    def play(self, url:str):
+        self.media = self.instance.media_new(url)
+        p = self.media.parse()
+        print('parse',p)
+        print('get_meta',self.media.get_meta(0))
+        self.mediaPlayer.set_media(self.media)
+        self.mediaPlayer.play()
